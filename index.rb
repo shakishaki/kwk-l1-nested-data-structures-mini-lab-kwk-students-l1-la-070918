be9e377d89d9b735faaf5ec922bb23fd3d89a52b
shakishaki <shaki.canchola@yahoo.com>
2018-07-16 20:23:18 +0000

    automatically backed up by learn

--- index.rb
+++ index.rb
@@ -1,39 +1,44 @@
 # example ingredients array
-ingredients = [
-    ["rye", "sourdough", "baguette"],
-    ["ham", "salami", "turkey"],
-    ["swiss", "munster", "cheddar"],
-    ["mayo", "mustard", "tabasco"]
-]
+# ingredients = [
+#     ["rye", "sourdough", "baguette"],
+#     ["ham", "salami", "turkey"],
+#     ["swiss", "munster", "cheddar"],
+#     ["mayo", "mustard", "tabasco"]
+# ]
 
-def sandwich_request(ingredients, order) 
-  # your code here!
-end
+# def sandwich_request(ingredients, order) 
+#   if sandwich_request(ingredients, ["rye", "salami", "cheddar"])
+#     puts true
+# elsif
+#     puts false 
+# end
 
-# example city_info hash
-cities = {
-  :new_york => { 
-    :mayor => "Bill DeBlasio",
-    :population => 8337000,
-    :website => "http://www.nyc.gov"
-  },
-  :los_angeles => { 
-    :mayor => "Eric Garcetti",
-    :population => 3884307,
-    :website => "http://www.lacity.org"
-  },
-  :miami => { 
-    :mayor => "Tomás Regalado",
-    :population => 419777,
-    :website => "http://www.miamigov.com"
-  },
-  :chicago => { 
-    :mayor => "Rahm Emanuel",
-    :population => 2695598,
-    :website => "http://www.cityofchicago.org/"
-  }
-}
+ # example city_info hash
+ cities = {
+   :new_york => { 
+     :mayor => "Bill DeBlasio",
+     :population => 8337000,
+     :website => "http://www.nyc.gov"
+   },
+   :los_angeles => { 
+     :mayor => "Eric Garcetti",
+     :population => 3884307,
+     :website => "http://www.lacity.org"
+   },
+   :miami => { 
+     :mayor => "Tomás Regalado",
+     :population => 419777,
+     :website => "http://www.miamigov.com"
+   },
+   :chicago => { 
+     :mayor => "Rahm Emanuel",
+     :population => 2695598,
+     :website => "http://www.cityofchicago.org/"
+   }
+ }
 
 def city_populations(cities)
-  # your code here!
+  puts miami{"population]"
 end
+
+
